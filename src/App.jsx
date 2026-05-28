78ee2aa40573f718b83490f046ae0b9ab73fbb88
Pranathi Balusu <bit.pranathi@gmail.com>
2026-05-28 17:29:29 +0530

    Saved current changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+`import { useState } from "react";
 import "./App.css";
 import Dashboard from "./Dashboard";
 import IssueDetail from "./IssueDetail";
@@ -318,4 +318,4 @@ export default function App() {
 
     </div>
   );
-}
+}`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
